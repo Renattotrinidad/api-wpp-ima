df358107c77e6cac411d365f3dd0ba5e731bb6b3
Renatto Gabriel Trinidad Lopez <a69364@MacBook-Pro-de-69364.local>
2024-10-11 11:39:48 -0600

    Se realiza una pequeña modificación en código

--- src/app.ts
+++ src/app.ts
@@ -2,39 +2,56 @@ import 'dotenv/config'
 import { createBot, createFlow, MemoryDB, createProvider, addKeyword } from '@bot-whatsapp/bot'
 import { BaileysProvider, handleCtx } from '@bot-whatsapp/provider-baileys'
 
-const flowBienvenida = addKeyword('hola' ).addAnswer('Buenas!! bienvenido')
+// const flowBienvenida = addKeyword('hola').addAnswer('Buenas!! bienvenido')
 /**
  * 
  */
 const main = async () => {
-    
-    const provider = createProvider(BaileysProvider)
 
-    provider.initHttpServer(3002)
+  const provider = createProvider(BaileysProvider)
 
-    if (provider.http) {
-        // Aquí puedes utilizar la propiedad "http" sin que se produzca un error
-        provider.http.server.post('/send-message', handleCtx(async (bot, req, res) => {
-          // Tu código aquí
-          const body = req.body
-          const message = body.message
-          const mediaUrl = body.mediaUrl
-          const phone = body.phone
-        //   console.log(body)
-          //await bot.sendMessage('573016083120', 'mensaje tukusito1!', {})
+  provider.initHttpServer(3002)
+
+  if (provider.http) {
+    // Aquí puedes utilizar la propiedad "http" sin que se produzca un error
+    provider.http.server.post('/send-message', handleCtx(async (bot, req, res) => {
+      
+      const body = req.body
+      const message = body.message
+      const mediaUrl = body.mediaUrl
+      const phone = body.phone
+
+      try {
+
+        if (!mediaUrl) {
+
+          await bot.sendMessage(phone, message, {});
+
+        } else {
+
           await bot.sendMessage(phone, message, {
-                 media: mediaUrl
-          })
-          //await bot.sendMessage(process.env.FRIENDNUMBER.toString(), 'mensaje tukusito!', {})
-        res.end('Mensaje enviado al número:' + phone)
-        }));
-    }
-   
-    await createBot ({
-        flow: createFlow ([flowBienvenida]),
-        database: new MemoryDB(),
-        provider
-    })
+            media: mediaUrl
+          });
+
+        }
+
+        res.end('Mensaje enviado correctamente a:' + phone);
+
+      } catch (error) {
+        
+        console.log(error);
+
+      }
+
+    }));
+
+  }
+
+  // await createBot({
+  //   flow: createFlow([flowBienvenida]),
+  //   database: new MemoryDB(),
+  //   provider
+  // })
 }
 
-main ()
+main()
